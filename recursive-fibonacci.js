1560dc6a14df937867b995fe8564a89a1b2c1352
danang sukma wantara <danangsukma@gmail.com>
2023-09-20 19:43:06 +0700

    upgrade memoization

--- recursive-fibonacci.js
+++ recursive-fibonacci.js
@@ -1,16 +1,24 @@
-function fibonacci(item) {
-    if(item < 2)return item;
+function fibonacci(item, mem = {}) {
+    console.log(item, mem);
 
-    return fibonacci(item-1) + fibonacci(item-2); 
+    if(item < 2) return item;
+  
+    if(item in mem) return mem[item];
+
+    mem[item] = fibonacci(item-1, mem) + fibonacci(item-2, mem); 
+
+    return mem[item];
 }
 
-console.log(fibonacci(1));
-console.log(fibonacci(2));
-console.log(fibonacci(3));
-console.log(fibonacci(4));
-console.log(fibonacci(5));
-console.log(fibonacci(6));
-console.log(fibonacci(7));
-console.log(fibonacci(8));
-console.log(fibonacci(9));
+// console.log(fibonacci(1));
+// console.log(fibonacci(2));
+// console.log(fibonacci(3));
+// console.log(fibonacci(4));
+// console.log(fibonacci(5));
+// console.log(fibonacci(6));
+// console.log(fibonacci(7));
+// console.log(fibonacci(8));
+// console.log(fibonacci(9));
 console.log(fibonacci(10));
+// console.log(fibonacci(20));
+// console.log(fibonacci(50)); 
