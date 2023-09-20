0949805aad2eb417a5df7be8aa7d7ded92a0d2e2
danang sukma wantara <danangsukma@gmail.com>
2023-09-20 18:47:21 +0700

    recursive fibonacci

---
+++ recursive-fibonacci.js
@@ -0,0 +1,16 @@
+function fibonacci(item) {
+    if(item < 2)return item;
+
+    return fibonacci(item-1) + fibonacci(item-2); 
+}
+
+console.log(fibonacci(1));
+console.log(fibonacci(2));
+console.log(fibonacci(3));
+console.log(fibonacci(4));
+console.log(fibonacci(5));
+console.log(fibonacci(6));
+console.log(fibonacci(7));
+console.log(fibonacci(8));
+console.log(fibonacci(9));
+console.log(fibonacci(10));
