a8773a7110fc88d94cc1395766668a9e5b2f2ba7
danang sukma wantara <danangsukma@gmail.com>
2023-10-01 06:36:04 +0700

    add more

---
+++ dsa/hashtable/hash.js
@@ -0,0 +1,10 @@
+/**
+ * Hashtable
+ * Unordered associative array
+ * Time complexity in in big O notation
+ * Algorithm    | Average   | Worst Case
+ * Space        | O(n)[1]   | O(n)
+ * Search       | O(1)      | O(n)
+ * Insert       | O(1)      | O(n)
+ * Delete       | O(1)      | O(n)
+ */
