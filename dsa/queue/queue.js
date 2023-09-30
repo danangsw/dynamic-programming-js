2e4563bb1b7c75b68dfa58babae6e1a9743f05ea
danang sukma wantara <danangsukma@gmail.com>
2023-09-30 13:10:38 +0700

    add more learning

---
+++ dsa/queue/queue.js
@@ -0,0 +1,45 @@
+/**
+ * Queue
+ * FIFO (First In First Out)
+ */
+
+function Queue(list = []) { 
+    let collection = [];
+
+    this.peek = () => { 
+        return collection;
+    }
+    this.enqueue = (e) => { 
+        collection.push(e);
+    }
+    this.dequeue = () => { 
+        return collection.shift();
+    }
+    this.front = () => { 
+        return collection[0];
+    }
+    this.tail = () => { 
+        return collection[this.size() - 1];
+    }
+    this.size = () => { 
+        return collection.length;
+    }
+    list.forEach(e => { 
+        this.enqueue(e);
+    })
+}
+
+const q = new Queue([4, 5, 6]);
+q.enqueue(1);
+q.enqueue(2);
+q.enqueue(3);
+console.log(q.peek(), q.size());
+q.dequeue();
+q.dequeue();
+q.dequeue();
+q.dequeue();
+q.dequeue();
+q.dequeue();
+console.log(q.peek(), q.front(), q.tail(), q.size());
+q.enqueue(0);
+console.log(q.peek(), q.front(), q.tail(), q.size());
