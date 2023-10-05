5b2e8a17b82fc7b1aec95eb54f31967db87a46fb
danang sukma wantara <danangsukma@gmail.com>
2023-10-05 14:58:31 +0700

    add more

--- sorting/linkedlist-merge-sort.js
+++ sorting/linkedlist-merge-sort.js
@@ -1,4 +1,6 @@
 import { SingleLinkedList } from '../dsa/linked-list/sll2.js';
 const sll = new SingleLinkedList();
-sll.insertFirst(200);
-console.log({sll});
+sll.insertFirst(100);
+sll.insertLast(300);
+sll.insertAt(200, 1);
+sll.printListData();
