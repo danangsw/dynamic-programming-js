f9f4edfd6ab15b0595335bcd8cf7c1554bc27d88
danang sukma wantara <danangsukma@gmail.com>
2023-10-15 21:22:01 +0700

    add more

--- basic/oneliner.js
+++ basic/oneliner.js
@@ -4,6 +4,12 @@
 
 // 1. How to Capitalize Text
 const capitalize = (str) => `${str.charAt(0).toUpperCase()}${str.slice(1)}`
-let str = 'javascript'
+const str = 'javascript'
 console.log(`1. How to Capitalize Text`)
 console.log(str, capitalize(str));
+
+// 2. How to Calculate Percent
+const percent = (value, total) => Math.round((value / total) * 100);
+const numbers = [10, 100]
+console.log(`2. How to Calculate Percent`);
+console.log({ numbers }, percent(numbers[0], numbers[1]));
