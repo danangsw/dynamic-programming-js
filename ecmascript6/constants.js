dbf7d0f4637ec9847e927a3884872995e8094fa0
danang sukma wantara <danangsukma@gmail.com>
2023-10-15 22:37:05 +0700

    add more

---
+++ ecmascript6/constants.js
@@ -0,0 +1,19 @@
+/**
+ * Constants
+ * Support for constants (also known ad "immutable variables"), i.e, variables which cannot be reassigned new value.
+ * Notice: this only makes the varible itself immutable, not its assigned content.
+ */
+
+const PI = 3.141593;
+const anObj = {};
+const items = [];
+
+anObj.id = 1;       // OK
+anObj.name = 'A';   // OK
+
+items.push(1);       // OK
+items.push(2);       // OK
+
+// items = [1,2,3] // error runtime: Assignment to constant variable.
+// anObj = {id: 2, name: 'B'}  // error runtime: Assignment to constant variable.
+// PI = 3.14; // error runtime: Assignment to constant variable.
