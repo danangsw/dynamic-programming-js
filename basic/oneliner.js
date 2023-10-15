bb1bf0b7b8f43321ca6172af820d29ff1f5a22d2
danang sukma wantara <danangsukma@gmail.com>
2023-10-15 21:25:34 +0700

    add more

--- basic/oneliner.js
+++ basic/oneliner.js
@@ -13,3 +13,9 @@ const percent = (value, total) => Math.round((value / total) * 100);
 const numbers = [10, 100]
 console.log(`2. How to Calculate Percent`);
 console.log({ numbers }, percent(numbers[0], numbers[1]));
+
+// 3. How to Get a Random Element
+const getRandomItems = (items) => items[Math.floor(Math.random() * items.length)];
+const items = ["Nicely done!", "Good job!", "Good work!", "Correct!", 100, 200, 300];
+console.log("3. How to Get a Random Element");
+console.log(getRandomItems(items));
