059dc2713bc2395af5c4876ba80c2986c88a8045
danang sukma wantara <danangsukma@gmail.com>
2023-10-30 09:13:00 +0700

    add more

---
+++ algorithm/min-distances.js
@@ -0,0 +1,31 @@
+/**
+ * https://www.hackerrank.com/challenges/minimum-distances/problem
+ * 
+ * Complete the 'minimumDistances' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY a as parameter.
+ */
+
+function minimumDistances(a) {
+    // Write your code here
+    let min = Number.MAX_SAFE_INTEGER;
+
+    a.forEach((e, i) => {
+        for (let j = (i + 1); j < a.length; j++) {
+            const v = a[j];
+             if (e === v) {
+                if (j - i < min) { 
+                    min = j - i;
+                }
+            }
+        }
+    });
+
+    return min === Number.MAX_SAFE_INTEGER ? -1 : min;
+}
+
+// Drive code
+console.log(minimumDistances([7, 1, 3, 4, 1, 7])); //. output: 3
+console.log(minimumDistances([3, 2, 1, 2, 3])); //. output: 2
+console.log(minimumDistances([1,2,3,4,10])); //. output: -1
