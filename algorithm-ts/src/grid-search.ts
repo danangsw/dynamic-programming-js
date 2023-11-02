72c6f7692e39c1267b8c56fa2def18ae7a20ad1b
danang sukma wantara <danangsukma@gmail.com>
2023-11-02 21:17:55 +0700

    add more

--- algorithm-ts/src/grid-search.ts
+++ algorithm-ts/src/grid-search.ts
@@ -13,36 +13,33 @@ function gridSearch(G: string[], P: string[]): string {
     let result: boolean = false;
     const matchedSet = new Set<number>();
     const pxlen = P.length;
-    const pylen = P[0].length;
-    const glen = G.length;
 
     G.forEach((e, i) => {
-        // console.log({ i, e }, e.search(p[0]));
-        if ( e.search(p[0]) > 0) matchedSet.add(i);
+        if ( e.search(P[0]) > -1) matchedSet.add(i);
     });
 
     for (const el of matchedSet.values()) {
-        const mi: number[] = [];
-        let isEq = false;
+        const mapMi = new Map();
         for (let i = el, j = 0; i < (el + pxlen); i++, j++) {
+            const idx: number[] = [];
             if (G[i]) { 
-                const ie = G[i].search(P[j]);
-                mi.push(ie);
-                isEq = mi[0] === ie ? true : false;
-                console.log(G[i], P[j])
+                let index = G[i].indexOf(P[j]);
+                while (index !== -1) {
+                    if (!mapMi.has(index))
+                        mapMi.set(index, 1);
+                    else
+                        mapMi.set(index, mapMi.get(index) + 1);
+
+                    if (mapMi.get(index) === pxlen) { 
+                        result = true;
+                        break;
+                    }
+                    index = G[i].indexOf(P[j], index + 1);
+                }
             }
         }
-        console.log('------', isEq && mi.length === pxlen);
     }
 
-    // for (let i = 0; i < glen - pxlen; i++) {
-    //     for (let j = i; j < pxlen+i; j++) {
-    //         console.log(G[j].slice(0, pylen))
-    //     }
-    //     console.log('------')
-    // }
-
-    // console.log(matchedSet)
     return result ? 'YES' : 'NO'
 }
 
@@ -50,7 +47,7 @@ let g: string[] = [];
 let p: string[] = [];
 
 g = [
-    '7283455864',
+    '9505455864',
     '6731158619',
     '8988242643',
     '3830589324',
@@ -66,14 +63,14 @@ p = [
     '9505',
     '3845',
     '3530',
-    '3106'
+    '3100'
 ]
-console.log(gridSearch(g, p));
+console.log(gridSearch(g, p)); // NO
 g = [
-    '400453592126560',
-    '114213133098692',
-    '474386082879648',
-    '522356951189169',
+    '400453592126599',
+    '994213133098692',
+    '994386099879699',
+    '522356999189199',
     '887109450487496',
     '252802633388782',
     '502771484966748',
@@ -90,4 +87,4 @@ p = [
     '99',
     '99'
 ]
-// console.log(gridSearch(g, p));
+console.log(gridSearch(g, p)); // YES
