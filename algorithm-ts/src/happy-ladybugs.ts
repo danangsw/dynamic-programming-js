93c79cf37aad8c602ca831e002b5dbb0c943e48c
danang sukma wantara <danangsukma@gmail.com>
2023-11-03 10:35:47 +0700

    add more

--- algorithm-ts/src/happy-ladybugs.ts
+++ algorithm-ts/src/happy-ladybugs.ts
@@ -8,36 +8,47 @@
  * string b: the initial positions and colors of the ladybugs
  */
 
-function happyLadybugs(b: string): string {
-    // Write your code here
-    const mapBug = new Map();
-
+function setMapBugs(b: string): Map<string, number[]> { 
+    const mapBug = new Map<string, number[]>();
+    // Generate map string index
     b.split('').forEach(e => {
         let index = b.indexOf(e);
         const indexes:number[] = []
         while (index !== -1) {
             indexes.push(index)
             mapBug.set(e, indexes);
-
             index = b.indexOf(e, index + 1);
         }
     });
-    
-    console.log(mapBug);
-    
 
-    let isHappy = false;
-    mapBug.forEach((v, k) => {
-        if (k === '_' && v.length > 0) isHappy = true;
-        if (k !== '_' && v > 1) isHappy = true;
-        else isHappy = false
-    })
+    return mapBug;
+}    
     
-    // const hasBuffer = b.indexOf('_') > -1;
-    // const countBuffer = b.matchAll(/\b_\b/g);
-    // b.split('').forEach(e => { 
+function happyLadybugs(b: string): string {
+    // Write your code here
+    let isHappy = true;
+    // Validate any buffer "_"
+    const hasBuffer = b.indexOf("_") < 0 ? false : true;
+    const bSorted = b.split('').sort().join('');
+    // Have no buffer "_"
+    // Validate the origin
+    // Have buffer "_"
+    // Then sort the origin
+    const mapBug = hasBuffer ? setMapBugs(bSorted) : setMapBugs(b);
+    // Logic engine
+    mapBug.forEach((v, k) => {
+        if (!isHappy) { 
+            return;
+        }
+
+        const vlen = v.length;
+        const [max, min] = [Math.max(...v), Math.min(...v)];
         
-    // });
+        if (k !== '_' &&
+            (vlen < 2 || (max - min) !== (vlen - 1))) {
+            isHappy = false;
+        }
+    });
 
     return isHappy ? 'YES' : 'NO';
 }
@@ -52,3 +63,6 @@ console.log(happyLadybugs('AABBC_C'));      // YES
 console.log(happyLadybugs('_'));            // YES
 console.log(happyLadybugs('DD__FQ_QQF'));   // YES
 console.log(happyLadybugs('AABCBC'));       // NO
+console.log(happyLadybugs('AABBCC'));       // YES
+console.log(happyLadybugs('___BB'));        // YES
+console.log(happyLadybugs('AB_______CD'));  // NO
