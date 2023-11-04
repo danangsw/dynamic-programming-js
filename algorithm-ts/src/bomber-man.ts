9b4ff0372339898838c0970dacfbbffc7220dcc7
danang sukma wantara <danangsukma@gmail.com>
2023-11-04 17:51:46 +0700

    add more

---
+++ algorithm-ts/src/bomber-man.ts
@@ -0,0 +1,38 @@
+/**
+ * https://www.hackerrank.com/challenges/bomber-man/problem
+ * 
+ * Complete the bomberMan function in the editory below.
+ * bomberMan has the following parameter(s):
+ * int n: the number of seconds to simulate
+ * string grid[r]: an array of strings that represents the grid
+ * 
+ * Returns
+ * string[r]: n array of strings that represent the grid in its final state
+ */
+
+function bomberMan(n: number, grid: string[]): string[] {
+    // Write your code here
+    return [];
+}
+
+// Driven code
+let tc: string[];
+
+tc = [
+    '.......',
+    '...O...',
+    '....O..',
+    '.......',
+    'OO.....',
+    'OO.....'
+];
+console.log(tc); // OOO.OOO, OO...OO, OOO...O, ..OO.OO, ...OOOO, ...OOOO
+
+tc = [
+    '...',
+    '.O.',
+    '...'
+];
+console.log(tc); // O.O, ..., O.O
+
+
