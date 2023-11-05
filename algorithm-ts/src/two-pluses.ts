b41d67b407e54cced3e142c8c9d146d8ac16c466
danang sukma wantara <danangsukma@gmail.com>
2023-11-05 16:26:17 +0700

    add more

---
+++ algorithm-ts/src/two-pluses.ts
@@ -0,0 +1,21 @@
+/**
+ * https://www.hackerrank.com/challenges/two-pluses/problem
+ * 
+ * Complete the twoPluses function in the editor below. 
+ * It should return an integer that represents the area of the two largest pluses.
+ * 
+ * The function accepts STRING_ARRAY grid as parameter
+ * an array of strings where each string represents a row and each character of the string represents a column of that row
+ *
+ * The function is expected to return an INTEGER.
+ *  
+ **/
+
+function twoPluses(grid: string[]): number {
+    // Write your code here
+    return 0;
+}
+
+// Drive code
+console.log(twoPluses(['GGGGGG', 'GBBBGB', 'GGGGGG', 'GGBBGB', 'GGGGGG'])); // 5
+console.log(twoPluses(['BGBBGB', 'GGGGGG', 'BGBBGB', 'GGGGGG', 'BGBBGB', 'BGBBGB'])); // 25
