c74a1cf9f525feb3ee5a1cbc55c743fd86cd3a2c
danang sukma wantara <danangsukma@gmail.com>
2023-11-06 11:33:23 +0700

    add more

--- algorithm-ts/src/two-pluses.ts
+++ algorithm-ts/src/two-pluses.ts
@@ -13,9 +13,31 @@
 
 function twoPluses(grid: string[]): number {
     // Write your code here
+    const glen = grid.length;
+    const mapGrid = new Map();
+
+    for (let i = 0; i < glen; i++) {
+        const item = grid[i];
+        
+        for (let j = 0; j < item.length; j++) {
+            const char = item[j];
+            let charIndexes: number[][] = [];
+
+            if (mapGrid.has(char)) { 
+                charIndexes = mapGrid.get(char);
+            }
+        
+            charIndexes.push([i, j])
+            mapGrid.set(char, charIndexes);
+        }
+    }
+
+    console.log(mapGrid);
+
     return 0;
 }
 
 // Drive code
 console.log(twoPluses(['GGGGGG', 'GBBBGB', 'GGGGGG', 'GGBBGB', 'GGGGGG'])); // 5
 console.log(twoPluses(['BGBBGB', 'GGGGGG', 'BGBBGB', 'GGGGGG', 'BGBBGB', 'BGBBGB'])); // 25
+console.log(twoPluses(['BGBBRB', 'GRGGGG', 'BGBBGB', 'RGGGGG', 'BRBBGB', 'BGBBRB'])); // 25
