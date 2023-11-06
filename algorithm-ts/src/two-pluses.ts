4d3cf16653869984a75d4e3dda7186ce32d4d2a3
danang sukma wantara <danangsukma@gmail.com>
2023-11-06 18:36:06 +0700

    add more

--- algorithm-ts/src/two-pluses.ts
+++ algorithm-ts/src/two-pluses.ts
@@ -10,31 +10,62 @@
  * The function is expected to return an INTEGER.
  *  
  **/
-
 function twoPluses(grid: string[]): number {
     // Write your code here
     const glen = grid.length;
+    const ilen = grid[0].length;
     const mapGrid = new Map();
+    const setGrid = new Set();
 
-    for (let i = 0; i < glen; i++) {
-        const item = grid[i];
-        
-        for (let j = 0; j < item.length; j++) {
-            const char = item[j];
-            let charIndexes: number[][] = [];
-
-            if (mapGrid.has(char)) { 
-                charIndexes = mapGrid.get(char);
-            }
-        
-            charIndexes.push([i, j])
-            mapGrid.set(char, charIndexes);
+    // Get the unique values
+    for (const item of grid) {
+        for (let i = 0; i < ilen; i++) {
+            setGrid.add(item[i]);
         }
     }
 
+    // Convert to numeric items
+    const defGridNumerics = String().padStart(ilen, '0')
+        .split('').map(c => parseInt(c));
+
+    for (const val of setGrid) {
+        let items: number[][] = [];
+        for (const item of grid) { 
+            let gridItems = [...defGridNumerics];            
+            let charAt = item.indexOf(val as string); 
+        
+            while (charAt !== -1) {
+                // Set detonate indexes
+                gridItems[charAt] = 1;
+                // Find next index           
+                charAt = item.indexOf(val as string, charAt + 1);
+            }
+
+            items.push(gridItems);
+        }
+
+        mapGrid.set(val, items)
+    }
     console.log(mapGrid);
 
-    return 0;
+    const maxPlus:number[] = [1, 1];
+    for (const [key, item] of mapGrid) { 
+        // Calculate the largest SUM of contiguos subarray
+       
+    }
+
+    return maxPlus[0] * maxPlus[1]
+}
+
+function maxTwoPlus(arr: number[][]): number[] { 
+    let max:number[] = [1, 1];
+
+    // Initializing all four arrays of left, right, up and down
+    // Initializing up left and up
+    // Initializing up right and down
+    // Calculating the value of Maximum PLUS (+) sign
+
+    return max;
 }
 
 // Drive code
