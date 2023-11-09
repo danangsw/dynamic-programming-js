8f5d81ea81daf0e2b22d332f8d82ce37d53f8591
danang sukma wantara <danangsukma@gmail.com>
2023-11-09 17:04:49 +0700

    add more

---
+++ algorithm-ts/src/larrys-array.ts
@@ -0,0 +1,13 @@
+/**
+ * https://www.hackerrank.com/challenges/larrys-array/problem
+ *  
+ * Complete the 'larrysArray' function below.
+ *
+ * The function is expected to return a STRING (YES or NO).
+ * The function accepts INTEGER_ARRAY A as parameter.
+ */
+
+function larrysArray(A: number[]): string {
+    // Write your code here
+    return 'NO';
+}
