f072de4eabbcfd66e79b6cd7d802ca4ef3aa2ab0
danang sukma wantara <danangsukma@gmail.com>
2023-11-12 17:32:52 +0700

    add more

---
+++ algorithm-ts/src/count-ways-1.ts
@@ -0,0 +1,19 @@
+/**
+ * https://www.hackerrank.com/challenges/count-ways-1/problem
+ * 
+ * Complete the 'countWays' function below.
+ * In this HackerRank Counting, the Ways problem solution Little Walter likes playing with his toy scales. He has N types of weights. The Ith weight type has weight Ai. There are infinitely many weights of each type.
+ * Recently, Walter defined a function, F(X), denoting the number of different ways to combine several weights so their total weight is equal to X. Ways are considered to be different if there is a type that has a different number of weights used in these two ways.
+ * 
+ * 
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY arr
+ *  2. LONG_INTEGER l
+ *  3. LONG_INTEGER r
+ */
+
+function countWays(arr: number[], l: number, r: number): number {
+    // Write your code here
+    return 0;
+}
