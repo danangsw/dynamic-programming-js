1d2ff758badc31848559e39330e8a4c087fa944d
danang sukma wantara <danangsukma@gmail.com>
2023-11-15 19:52:31 +0700

    add more

---
+++ algorithm-ts/src/dsa/almost-equal-advanced.ts
@@ -0,0 +1,17 @@
+/**
+ * https://www.hackerrank.com/challenges/almost-equal-advanced/problem
+ * 
+ * Complete the 'solve' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY h: The first line contains an integer N and K separated by a single space representing the number of Sumo wrestlers who are going to participate and the height difference K.
+ *  2. 2D_INTEGER_ARRAY queries
+ */
+
+function solve(n: number, k: number, h: number[], queries: number[][]): number[] {
+    // Write your code here
+    return [];
+}
+
+console.log(solve(5, 2, [ 1, 3, 4, 3, 0 ], [ [ 0, 1 ], [ 1, 3 ], [ 0, 4 ] ])) // [1, 3, 6]
